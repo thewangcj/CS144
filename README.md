d66e82dfc2402676635f88d51f9b67d18bec4027
thewangcj <thewangcj@gmail.com>
2023-05-25 16:34:03 +0800

    refactor: 整理 lab code

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # CS144
- 记录学习斯坦福CS144计算机网络的一切
+ 记录学习斯坦福CS144:fall-2021 计算机网络的一切
 ## 参考资料
 1. [官网](https://cs144.github.io/)
 2. [khanhnamle1994/computer-networking: Lecture Slides for Philip Levis and Nick McKeown's "Introduction to Computer Networking" Stanford course (github.com)](https://github.com/khanhnamle1994/computer-networking)
+3. [PKUFlyingPig](https://github.com/PKUFlyingPig/CS144-Computer-Network)
+4. [lab doc](https://github.com/endless-hu/CS144-Public)
 
--- sponge/apps/webget.cc
+++ sponge/apps/webget.cc
@@ -8,7 +8,15 @@ using namespace std;
 
 void get_URL(const string &host, const string &path) {
     // Your code here.
-
+    TCPSocket socket;
+    socket.connect(Address(host, "http"));
+    socket.write("GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\n" + "Connection: close" + "\r\n\r\n");
+    // socket.shutdown(SHUT_WR);
+    while (!socket.eof()) {
+        auto recvd = socket.read();
+        cout << recvd;
+    }
+    socket.close();
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
@@ -17,8 +25,8 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
