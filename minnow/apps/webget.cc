be79f92b356bb0ca68ed58f5a854dcb480551c6b
thewangcj <thewangcj@gmail.com>
2023-07-24 16:43:38 +0800

    rm sponge and use minnow, finish lab0

---
+++ minnow/apps/webget.cc
@@ -0,0 +1,55 @@
+#include "socket.hh"
+
+#include <cstdlib>
+#include <iostream>
+#include <span>
+#include <string>
+
+using namespace std;
+
+void get_URL( const string& host, const string& path )
+{
+  // Your code here.
+  TCPSocket socket;
+  socket.connect( Address( host, "http" ) );
+  socket.write( "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\n" + "Connection: close" + "\r\n\r\n" );
+  // socket.shutdown(SHUT_WR);
+  string recvd;
+  while ( !socket.eof() ) {
+    socket.read( recvd );
+    cout << recvd;
+  }
+  socket.close();
+}
+
+int main( int argc, char* argv[] )
+{
+  try {
+    if ( argc <= 0 ) {
+      abort(); // For sticklers: don't try to access argv[0] if argc <= 0.
+    }
+
+    auto args = span( argv, argc );
+
+    // The program takes two command-line arguments: the hostname and "path" part of the URL.
+    // Print the usage message unless there are these two arguments (plus the program name
+    // itself, so arg count = 3 in total).
+    if ( argc != 3 ) {
+      cerr << "Usage: " << args.front() << " HOST PATH\n";
+      cerr << "\tExample: " << args.front() << " stanford.edu /class/cs144\n";
+      return EXIT_FAILURE;
+    }
+
+    // Get the command-line arguments.
+    const string host { args[1] };
+    const string path { args[2] };
+
+    // Call the student-written function.
+    get_URL( host, path );
+  } catch ( const exception& e ) {
+    cerr << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
