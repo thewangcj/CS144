7b1dd01bc64ec09f065e0c3097e3822f5f3d95be
thewangcj <thewangcj@gmail.com>
2023-10-11 16:10:38 +0800

    lab2: finish wrap function

---
+++ lab_notes/lab2.md
@@ -0,0 +1,4 @@
+1. 之前代码中表示 index 是使用 64 位 int 表示,而 tcp header 为了节省开销考虑使用的是 32 位
+    - index 32位可能导致环绕情况出现,即 int 越界然后重新从0开始
+    - 之前默认 index 从 0 开始,实际上现实中 index 是随机数, index 表示的 SYN,所以第一个数据 index + 1
+    - SYN 和 FIN 虽然占用了 sequence number,但是实际上没有数据
--- sponge/libsponge/wrapping_integers.cc
+++ sponge/libsponge/wrapping_integers.cc
@@ -6,17 +6,14 @@
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return isn + n; }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +26,24 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    // 这里实际上就是把一个 32 位循环数转成一个 64 位真实值，checkout 是上一次的值
+    // 两个 seq 之间不会超过 1<<32，除非发生了错误
+    // 注意：最终结果是离 checkpoint 最近的 seq，seq 可能比 checkpoint 小
+    /*
+    // 找到n和checkpoint之间的最小步数
+    int32_t min_step = n - wrap(checkpoint, isn);
+    // 将步数加到checkpoint上
+    int64_t ret = checkpoint + min_step;
+    // 如果反着走的话要加2^32
+    return ret >= 0 ? static_cast<uint64_t>(ret) : ret + (1ul << 32);
+    */
+    uint32_t offset = n - isn;
+    // 先靠近 checkpoint
+    uint64_t res = offset + ((checkpoint >> 32) << 32);
+    // 判断是往前还是往后
+    if (abs(int64_t(res + (1ul << 32) - checkpoint)) < abs(int64_t(res - checkpoint)))
+        res += (1ul << 32);
+    if (res >= (1ul << 32) && abs(int64_t(res - (1ul << 32) - checkpoint)) < abs(int64_t(res - checkpoint)))
+        res -= (1ul << 32);
+    return res;
 }
